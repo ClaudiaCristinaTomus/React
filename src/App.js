32f550cd782e7c90625a8cf5514c63b642bb1c57
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-04-09 19:32:23 +0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 
+
+import Layout from "./components/Layout";
+import {Switch,Route} from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Footer from "./pages/Footer";
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <About/>
+        </Route>
+        <Route path="/" exact>
+          <Projects/>
+        </Route>
+        <Route path="/" exact>
+          <Footer/>
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
